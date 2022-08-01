45b535c82f22738d5cda1a2f95847201f9fdb164
JackySummer <1106619871@qq.com>
2022-08-01 21:38:23 +0800

    feat: init project

---
+++ react-router/README.md
@@ -0,0 +1,3 @@
+# 手写实现 react-router v5 版本
+
+https://github.com/remix-run/react-router/tree/v5.3.0
---
+++ react-router/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Detail from './components/Detail'
+import List from './components/List'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Route path="/home" component={Home}></Route>
+    <Route path="/detail" component={Detail}></Route>
+    <Route path="/list" component={List}></Route>
+  </BrowserRouter>
+)
