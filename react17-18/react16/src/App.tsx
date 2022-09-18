a568376a261207ccba3841c0339f43200fd15275
JackySummer <1106619871@qq.com>
2022-09-18 21:50:39 +0800

    feat: add remove event pooling example

--- react17-18/react16/src/App.tsx
+++ react17-18/react16/src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div className="App"></div>
+  const handleClick = (e: React.MouseEvent) => {
+    console.log('直接打印e', e.target) // <button>React事件池</button>
+    // v17以下在异步方法拿不到事件e，必须先调用 e.persist()
+    // e.persist()
+
+    setTimeout(() => {
+      console.log('setTimeout打印e', e.target) // null
+    })
+  }
+  return (
+    <div className="App">
+      <button onClick={handleClick}>React事件池</button>
+    </div>
+  )
 }
 
 export default App
--- react17-18/react17/src/App.tsx
+++ react17-18/react17/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className="App"></div>
+  // v17：去除了 React 事件池
+  const handleClick = (e: React.MouseEvent) => {
+    console.log('直接打印e', e.target) // <button>React事件池</button>
+
+    setTimeout(() => {
+      console.log('setTimeout打印e', e.target) // <button>React事件池</button>
+    })
+  }
+  return (
+    <div className="App">
+      <button onClick={handleClick}>React事件池</button>
+    </div>
+  )
 }
 
 export default App
