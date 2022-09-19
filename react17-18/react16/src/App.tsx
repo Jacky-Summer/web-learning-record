f7d343cb10fb2899ed2df6cf9706e914291cafd7
JackySummer <1106619871@qq.com>
2022-09-19 22:30:27 +0800

    feat: add event delegation change example

--- react17-18/react16/src/App.tsx
+++ react17-18/react16/src/App.tsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react'
+import Test from './Test'
+
 function App() {
+  const [isShowText, setIsShowText] = useState(false)
   const handleClick = (e: React.MouseEvent) => {
     console.log('直接打印e', e.target) // <button>React事件池</button>
     // v17以下在异步方法拿不到事件e，必须先调用 e.persist()
@@ -8,9 +12,25 @@ function App() {
       console.log('setTimeout打印e', e.target) // null
     })
   }
+
+  const handleShowText = (e: React.MouseEvent) => {
+    e.nativeEvent.stopImmediatePropagation() // 阻止监听同一事件的其他事件监听器被调用
+    setIsShowText(true)
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', () => {
+      setIsShowText(false)
+    })
+  }, [])
+
   return (
     <div className="App">
       <button onClick={handleClick}>React事件池</button>
+      <button onClick={handleShowText}>事件委托变更</button>
+
+      {isShowText && <div>展示文字</div>}
+      <Test />
     </div>
   )
 }
--- react17-18/react17/src/App.tsx
+++ react17-18/react17/src/App.tsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from 'react'
+import Test from './Test'
+
 function App() {
+  const [isShowText, setIsShowText] = useState(false)
+
   // v17：去除了 React 事件池
   const handleClick = (e: React.MouseEvent) => {
     console.log('直接打印e', e.target) // <button>React事件池</button>
@@ -7,9 +12,25 @@ function App() {
       console.log('setTimeout打印e', e.target) // <button>React事件池</button>
     })
   }
+
+  const handleShowText = (e: React.MouseEvent) => {
+    e.stopPropagation() // 停止向document传播
+    setIsShowText(true)
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', () => {
+      setIsShowText(false)
+    })
+  }, [])
+
   return (
     <div className="App">
       <button onClick={handleClick}>React事件池</button>
+
+      <button onClick={handleShowText}>事件委托变更</button>
+      {isShowText && <div>展示文字</div>}
+      <Test />
     </div>
   )
 }
